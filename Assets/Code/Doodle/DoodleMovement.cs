85e66d0a6e37482058884823a989f11f7d873409
luver4ever <danil.testov@gmail.com>
2022-07-19 13:18:59 +0600

    Add Some New Physics
    But it dont work yet :3 Kill me pls

--- Assets/Code/Doodle/DoodleMovement.cs
+++ Assets/Code/Doodle/DoodleMovement.cs
@@ -3,38 +3,54 @@ using UnityEngine;
 
 [RequireComponent(typeof(DoodleInput))]
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MovementPhysics))]
 public class DoodleMovement : MonoBehaviour 
 {
     private DoodleInput _input;
     private Rigidbody _rigidbody;
+    private MovementPhysics _rb;
     
 
     private void Awake()
     {
         _input = GetComponent<DoodleInput>();
         _rigidbody = GetComponent<Rigidbody>();
+        _rb = GetComponent<MovementPhysics>();
     }
     private void OnEnable()
     {
         _input.PositionChanged += OnPositionChange;
+        //_rb.PlatformCollisionEntered += OnColliderEnter;
     }
 
     private void OnDisable()
     {
         _input.PositionChanged -= OnPositionChange;
+        //_rb.PlatformCollisionEntered -= OnColliderEnter;
     }
 
     private void OnPositionChange(Vector3 position)
     {
-        _rigidbody.position += position;
+        transform.position += position;
     }
+
+   // private void OnColliderEnter(Collider collision)
+   // {
+   //    if (collision.gameObject.TryGetComponent(out Platform platform))
+   //    {
+   //         _rb.AddVelocity(new Vector3(0, SetJumpModify(platform.Bouncness), 0));
+   //         Debug.Log("Itouch");
+   //     }
+   // }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent(out Platform platform))
-            _rigidbody.velocity = new Vector3(0, SetJumpModify(platform.Bouncness),0);
+        {
+            _rigidbody.velocity = new Vector3(0, SetJumpModify(platform.Bouncness), 0);
+            Debug.Log("Itouch");
+        }
     }
-
     public float SetJumpModify(float delta)
     {
         return delta;
---
+++ Assets/Code/Doodle/MovementPhysics.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPhysics : MonoBehaviour
+{
+    [Header("Physics Settings")]
+    [SerializeField] private float _gravityScale = 9.82f;
+    [SerializeField] private Vector3 _force;
+    [SerializeField] private float _massInKilo = 1f;
+    [Header("Raycast Settings")]
+    [SerializeField] private float _collisionDetectDistance;
+    [SerializeField] private LayerMask _platformLayer;
+    [SerializeField] private float _rayDistance;
+    [SerializeField] private Transform _rayPoint;
+
+    private Vector3 _acceleration, _velocity, _position;
+
+    public event Action<Collider> PlatformCollisionEntered;
+
+    private void OnValidate()
+    {
+        if (_gravityScale < 0.1f)
+            _gravityScale = 0.1f;
+        if (_massInKilo < 0.1f)
+            _massInKilo = 0.1f;
+
+        if (_collisionDetectDistance < 0.1f)
+            _collisionDetectDistance = 0.1f;
+        if (_rayDistance < 0.1f)
+            _rayDistance = 0.1f;
+
+
+    }
+    private void FixedUpdate()
+    {
+        _acceleration = _force / _massInKilo * _gravityScale;
+
+        _velocity += _acceleration * Time.fixedDeltaTime;
+
+        _velocity -= _velocity * _velocity.magnitude * 0.5f * 10f * Time.fixedDeltaTime;
+
+        _position += _velocity * Time.fixedDeltaTime;
+
+        CheckCollision();
+
+        transform.position += _position;
+    }
+
+    private void CheckCollision()
+    {
+        var ray = new Ray(_rayPoint.position, Vector3.down * _rayDistance);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, _rayDistance, _platformLayer))
+        {
+            if (Mathf.Abs((_rayPoint.position - hit.transform.position).magnitude) < _collisionDetectDistance)
+            {
+                Debug.Log("ITouch");
+                PlatformCollisionEntered?.Invoke(hit.collider);
+            }
+        }
+    }
+    public void AddVelocity(Vector3 velocity)
+    {
+        _velocity += velocity;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(new Ray(_rayPoint.position, Vector3.down * 10));
+    }
+}
